ce30e4dc3c66fd8fa4a5ab1dbf760d1b7bbb0ed6
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-20 17:36:06 +0300

    v1: build parent class person

--- src/App.java
+++ src/App.java
@@ -1,4 +1,14 @@
 public class App {
+    abstract static class Person{
+        protected String name;
+        protected int age;
+        Person(String name, int age){
+            this.name = name;
+            this.age = age;
+        }
+        abstract void showInfo();
+    }
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
